2c75c60a69e6309bb195a8e6613267f8b5cf016d
cindy111111 <1551241382@qq.com>
2018-08-31 18:43:49 +0800

    创建分支
    创建分支时间为20180831

---
+++ src/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM, {Component}from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+
+ 
+ ReactDOM.render(
+ 	<div>
+ 		<i className="fa fa-address-book-o"></i>
+ 		<i className="fa fa-spin fa-spinner"></i>
+ 		<h1>Hello !</h1>
+ 	</div>,
+  document.getElementById('app')
+);
+
+
+
+
+
